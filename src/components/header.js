1774dfaf6d295efe554e3e7e3eefc4aa9e0d72b0
Jan Winterfeld <jan@markenbegeisterung.de>
2019-04-01 11:58:06 +0200

    install stylus

--- src/components/header.js
+++ src/components/header.js
@@ -2,13 +2,10 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import styles from "./header.module.styl"
+
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
+  <header className={styles.siteHeader}>
     <div
       style={{
         margin: `0 auto`,
