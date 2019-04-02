516688897e1379535bf7c8a291bd034d81506150
Jan Winterfeld <jan@markenbegeisterung.de>
2019-04-02 12:04:44 +0100

    layout component test

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,8 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import "./normalize.css"
+import styles from "./layout.module.styl"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -26,14 +27,7 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
+        <div className={styles.innerContainer}>
           <main>{children}</main>
           <footer>
             © {new Date().getFullYear()}, Built with
