65f1e71251bd9d0260d395547fa113669f04e709
Jan Winterfeld <jan@markenbegeisterung.de>
2019-04-03 11:22:19 +0100

    mobile positioning, layer

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,25 +4,28 @@ import { Link } from "gatsby"
 import styles from "./navigation.module.styl"
 
 const Navigation = () => (
-  <nav className={styles.pageNavigation}>
-    <ul>
-      <li className={styles.entry}>
-        <Link to="#">Euphorika</Link>
-      </li>
-      <li className={styles.entry}>
-        <Link to="#">Leistungen</Link>
-      </li>
-      <li className={styles.entry}>
-        <Link to="#">Best Cases</Link>
-      </li>
-      <li className={styles.entry}>
-        <Link to="#">Jobs</Link>
-      </li>
-      <li className={styles.entry}>
-        <Link to="#">Kontakt</Link>
-      </li>
-    </ul>
-  </nav>
+  <div className={styles.pageNavigation}>
+    <nav>
+      <ul>
+        <li className={styles.entry}>
+          <Link to="#">Euphorika</Link>
+        </li>
+        <li className={styles.entry}>
+          <Link to="#">Leistungen</Link>
+        </li>
+        <li className={styles.entry}>
+          <Link to="#">Best Cases</Link>
+        </li>
+        <li className={styles.entry}>
+          <Link to="#">Jobs</Link>
+        </li>
+        <li className={styles.entry}>
+          <Link to="#">Kontakt</Link>
+        </li>
+      </ul>
+    </nav>
+    <div className={styles.closeNavigation}>X</div>
+  </div>
 )
 
 export default Navigation
