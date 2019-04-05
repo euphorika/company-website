6422443edd93412d501ae524c1d78258cf85562d
Hussein Bawab <hussein@markenbegeisterung.de>
2019-04-05 14:15:05 +0200

    Header reset due to missunderstanding the tasks described in the Ticket

--- src/components/header.js
+++ src/components/header.js
@@ -6,9 +6,21 @@ import styles from "./header.module.styl"
 
 const Header = ({ siteTitle }) => (
   <header className={styles.siteHeader}>
-    <div className={styles.headerContainer}>
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
       <h1 style={{ margin: 0 }}>
-        <Link to="/" className={styles.headerLink}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
           {siteTitle}
         </Link>
       </h1>
