c3ead315528d81d267244ef6b59e0f1e8f1c967b
Hussein Bawab <hussein@markenbegeisterung.de>
2019-04-09 01:52:31 +0200

    Scroll Snap Css-Based

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,15 +1,23 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import styles from "./page-2.module.styl"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <div className={styles.container}>
+      <div>
+        <h1>Hi from the second page</h1>
+        <p>Welcome to page 2</p>
+        <Link to="/">Go back to the homepage</Link>
+      </div>
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+    </div>
   </Layout>
 )
 
