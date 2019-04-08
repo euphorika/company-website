e2f70181880b95bcd5967cd2a97ccfceabee7cde
Jan Winterfeld <jan@markenbegeisterung.de>
2019-04-08 11:13:55 +0100

    boilerplate

---
+++ src/components/fullpage.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import styles from "./fullpage.module.styl"
+
+const FullPage = () => (
+  <div className={styles.fullPage}>
+    FullPage
+  </div>
+)
+
+export default FullPage
