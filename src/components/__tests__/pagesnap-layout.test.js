433829f304bde46af3524fedb74d9b7e560fc605
Jan Winterfeld <jan@markenbegeisterung.de>
2019-04-19 10:40:36 +0100

    pagesnap layout test

---
+++ src/components/__tests__/pagesnap-layout.test.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { shallow } from "enzyme"
+import toJson from 'enzyme-to-json'
+
+import { useStaticQuery } from "gatsby"
+import PageSnapLayout from "../pagesnap-layout"
+
+beforeEach(() => {
+  useStaticQuery.mockReturnValueOnce({
+    site: {
+      siteMetadata: {
+        title: `Default Starter`
+      }
+    }
+  })
+})
+
+describe("PageSnapLayout", () => {
+  it("renders correctly", () => {
+    const PageSnapLayoutElement = (
+      <PageSnapLayout seoTitle="SEO Title">
+        <div>Children</div>
+      </PageSnapLayout>
+    )
+
+    const wrapper = shallow(PageSnapLayoutElement)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+})
