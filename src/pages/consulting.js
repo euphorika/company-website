510130587e6b9eb200bb7cd32f4c127ec80ea022
Jan Winterfeld <jan@markenbegeisterung.de>
2019-04-25 11:13:17 +0100

    Tile links and text positioning in fullpage sections

---
+++ src/pages/consulting.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import PageSnapLayout from "../components/pagesnap-layout"
+import FullPage from "../components/fullpage"
+
+const ConsultingPage = () => (
+  <PageSnapLayout title="Consulting">
+    <FullPage headerFontColor="#000" backgroundColor="#fff">
+      <h1>Consulting</h1>
+    </FullPage>
+  </PageSnapLayout>
+)
+
+export default ConsultingPage
