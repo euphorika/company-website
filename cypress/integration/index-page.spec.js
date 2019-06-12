b02be26d0badff0f208c8a129aebe558d7931cfc
Jan Winterfeld <jan@markenbegeisterung.de>
2019-06-12 18:01:48 +0200

    cypress test check fullsize

--- cypress/integration/index-page.spec.js
+++ cypress/integration/index-page.spec.js
@@ -1,6 +1,10 @@
 describe("Index Page Test", () => {
   it("Visit Index Page", () => {
+    const WIDTH = 1024
+    const HEIGHT = 768
+
     cy.visit("/")
+    cy.viewport(WIDTH, HEIGHT)
     cy.get(".pagesnap-module--page-snap-container--3nLP_")
       .find(".fullpage-module--full-page--AXGF8")
       .its("length")
@@ -8,6 +12,8 @@ describe("Index Page Test", () => {
     cy.get("footer")
       .scrollIntoView()
       .should("be.visible")
+      .should("have.css", "width", WIDTH + "px")
+      .should("have.css", "height", HEIGHT + "px")
     cy.get("header .header-module--logo--2PcjR").should("be.visible")
     cy.get("header nav").should("be.visible")
   })
