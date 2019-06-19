aec37d33a3ed005688471c1759b6a4970d75d569
Jan Winterfeld <jan@markenbegeisterung.de>
2019-06-19 16:36:28 +0200

    design video background, rm example diver video

--- src/components/__tests__/video.test.js
+++ src/components/__tests__/video.test.js
@@ -6,7 +6,7 @@ import Video from "../video"
 
 describe("Video", () => {
   it("renders correctly", () => {
-    const wrapper = shallow(<Video  video="/videos/diver.mp4" poster="/videos/diver.jpg" />)
+    const wrapper = shallow(<Video  video="/videos/design-video.m4v" poster="/videos/design-poster.jpg" />)
     expect(toJson(wrapper)).toMatchSnapshot()
   })
 })
